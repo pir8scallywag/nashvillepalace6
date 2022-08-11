6c9717e2528caf1025f6e9e74a857a1f66d56e48
pir8scallywag <jameskinlaw78@yahoo.com>
2022-08-10 20:57:05 -0400

    Daily forecast Container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 // Get form element value
 let leftColumnEL = document.querySelector("#left-column")
+
+// Get all the elements of cities list for event handler
+let citiesListContainerBtnEl = document.querySelector(".list-group-item");
+// Daily forecast Container
+let dailyWeatherContainerEl = document.querySelector("#forecast-output-container"); 
