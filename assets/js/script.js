49cf7ab45c45efb1db508a96478bda16ec4dbb92
pir8scallywag <jameskinlaw78@yahoo.com>
2022-08-10 20:59:58 -0400

    input and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,22 @@ let dynFormContainer = document.createElement("form");
 dynFormContainer.setAttribute("id", "dymCityForm");
 dynFormContainer.classList = "city-search-forecast-container";
 leftColumnEL.appendChild(dynFormContainer)
+
+// Create H3 element
+let formH3 = document.createElement("h3");
+formH3.textContent = " Search for a City ";
+dynFormContainer.appendChild(formH3);
+
+// Create input element
+let formInput = document.createElement("input");
+formInput.setAttribute("id", "city-name")
+formInput.setAttribute("type", "text");
+formInput.setAttribute("autofocus", "true");
+formInput.classList = "form-input";
+dynFormContainer.appendChild(formInput);
+
+// Create button element
+let formButton = document.createElement("button");
+formButton.setAttribute("type", "submit");
+formButton.classList= ("btn fas fa-search");
+dynFormContainer.appendChild(formButton);
