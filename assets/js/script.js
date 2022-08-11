6be8f28077778ca2112075a1082fadb600e0bce6
pir8scallywag <jameskinlaw78@yahoo.com>
2022-08-10 20:58:27 -0400

    form container

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ let leftColumnEL = document.querySelector("#left-column")
 let citiesListContainerBtnEl = document.querySelector(".list-group-item");
 // Daily forecast Container
 let dailyWeatherContainerEl = document.querySelector("#forecast-output-container"); 
+
+// Create a form container and containing elements
+let dynFormContainer = document.createElement("form");
+dynFormContainer.setAttribute("id", "dymCityForm");
+dynFormContainer.classList = "city-search-forecast-container";
+leftColumnEL.appendChild(dynFormContainer)
